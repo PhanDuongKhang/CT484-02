24528d10c0091e53c34b32433db56e72c529cc7c
Phan Duong Khang <khangb1910389@student.ctu.edu.vn>
2022-09-13 19:45:38 +0700

    Xay dung trang tong quan cac san pham

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ui/products/products_manager.dart'; 
 import 'ui/products/product_detail_screen.dart';
+import 'ui/products/product_overview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,10 +37,14 @@ class MyApp extends StatelessWidget {
       //   color: Colors.green,
       // ),
 
-      home: SafeArea(
-        child: ProductDetailScreen(
-          ProductsManager().items[0], 
-        ), 
+      // home: SafeArea(
+      //   child: ProductDetailScreen(
+      //     ProductsManager().items[0], 
+      //   ), 
+      // ),
+
+      home: const SafeArea(
+        child: ProductOverviewScreen(),
       ),
 
     );
