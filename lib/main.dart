e62ba6966b19408001699b9e0230e8f5cc196760
Phan Duong Khang <khangb1910389@student.ctu.edu.vn>
2022-09-19 15:38:50 +0700

    Xay dung trang gio hang

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'ui/products/products_manager.dart';
 import 'ui/products/product_detail_screen.dart';
 import 'ui/products/product_overview_screen.dart';
 import 'ui/products/user_products_screen.dart';
+import 'ui/cart/cart_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -48,8 +49,12 @@ class MyApp extends StatelessWidget {
       //   child: ProductOverviewScreen(),
       // ),
 
+      // home: const SafeArea(
+      //   child: UserProductsScreen(),
+      // ),
+
       home: const SafeArea(
-        child: UserProductsScreen(),
+        child: CartScreen(),
       ),
 
     );
